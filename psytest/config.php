0902cc2eec78f93c05c5260bbf7dbd3b48ed70cf
chen <sniperking@126.com>
2015-12-09 16:27:54 +0800

    123

--- psytest/config.php
+++ psytest/config.php
@@ -3,6 +3,6 @@
     $mysql_server_name="localhost:3306"; //数据库服务器名称
     $mysql_username="root"; // 连接数据库用户名
     $mysql_password="123456"; // 连接数据库密码
-    $mysql_database="psytest";   // 数据库的名字11111111111
+    $mysql_database="psytest";   // 数据库的名字222222222222
     $debug = 1; 
 ?>
