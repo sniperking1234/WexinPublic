748f28b5b95b4cc1fb7dfc3b79aa9e7a8e261f21
Sia <807395550@>
2015-12-09 16:32:20 +0800

    1112

--- psytest/config.php
+++ psytest/config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    $mysql_server_name="localhost:3306"; //数据库服务器名称000000000000000000000
+    $mysql_server_name="localhost:3306"; //数据库服务器名称000000999
     $mysql_username="root"; // 连接数据库用户名
     $mysql_password="1018"; // 连接数据库密码
     $mysql_database="psytest"; // 数据库的名字
