424746152a28d03c39d0132d24029c336e82807c
chen <sniperking@126.com>
2015-12-20 12:30:05 +0800

    add function

--- psytest/BLL/makePaperList.php
+++ psytest/BLL/makePaperList.php
@@ -1,12 +1,29 @@
 <?php
 /*
  * @chen 
- * 获取所有的试题列表
+ * 获取所有的试题列表，包含以下方法
+ * GetAllPaper()  获取所有试题列表
+ * SearchPaper($paperName)   进行模糊查询
+ * GetTypePaper($typeName)   获取某一类型的所有试卷
  */
 
-//获取所有试题列表
-function GetAllPaperList()
+/*获取所有试题列表*/
+function GetAllPaper()
 {
     include_once '../DAL/paperDal.php';
     return FindALLPaper();
+}
+
+/*进行模糊查询*/
+function SearchPaper($paperName)
+{
+    include_once '../DAL/paperDal.php';
+    return FindPaperLikePaperName($paperName);
+}
+
+/*获取某一类型的所有试卷*/
+function GetTypePaper($typeName)
+{
+    include_once '../DAL/paperDal.php';
+    return FindPaperByPaperType($typeName);
 }
